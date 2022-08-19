4a6177c664454324dc4db0a1efa7b9e251ba609b
unknown <mohasanimam@gmail.com>
2022-08-19 23:44:55 +0600

    dynamic checkout route done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,15 +10,41 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.petxb8a.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("database connected");
-  client.close();
-});
+console.log(uri);
+console.log(uri);
+
+
+async function run() {
+    try {
+        await client.connect();
+
+      const menuItemsCollection = client.db("redonion").collection('menuitems');
+
+      // loading all menu items in menu page
+      app.get('/menuitems',async(req, res)=>{
+        const menuitems = await menuItemsCollection.find().toArray();
+        res.send(menuitems);
+      })
+
+      // loading single menu item in checkout page
+      app.get('/checkout/:id',async(req, res)=>{
+        console.log(req.params);
+        const id = req.params;
+        const query = { _id:ObjectId(id)};
+        const menuItem = await menuItemsCollection.findOne(query);
+        res.send(menuItem);
+      })
+
+
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 
 
